dbdc52d5c68503dee01217c5a7f467218dc46fbe
Christopher L Chapman <ChristopherLChapman42@gmail.com>
2018-05-02 16:41:27 -0700

    Day 3 lab: add Q6 & Q7

--- js/app.js
+++ js/app.js
@@ -70,3 +70,66 @@ if (answer5 === 'y' || answer5 === 'yes') {
     alert('Oh man, if you were correct than I would be hating my life!');
     console.log('user thinks I have hated most of my life (Q5)');
 }
+
+//ask user the sixth question, with 4 guesses with high low feedback to get a correct number
+var toManyTries = 5;
+// loop the question and answer testing, interate and say how many tries they have left or exit if they are correct
+var i = 1; 
+
+while (i < toManyTries) {
+    var answer6 = prompt('How many years have I lived in my current neighborhood?');
+    // [attempt ' + i + ' out of ' totalTries + '] ');
+    console.log('user answered '+ answer6 + ' on attempt ' + i + ' for Q6');
+    // test if they got it right on this try, or tell them if they are under or over. 
+    if (answer6 === '6') {
+        alert('You are correct! You figured that out in ' + i + ' attempts');
+        console.log('user is correct on attempt ' + i + '!');
+        i = toManyTries;
+    } else if (answer6 > 6) {
+        alert('Oh, it is less than that.');
+        console.log('user guess is too high on attempt ' + i + '. ');
+        i++;
+    } else if (answer6 < 6) { // currently a null response is recored the same as a '0' and counts as a guess
+        alert('Oh it is more than that. ');
+        console.log('user guess is too low on attempt ' + i + '. ');
+        i++;
+    } else {
+        alert('Hey, your answer was not in the right format. Try giving an integer');
+        console.log('user did not give a valid answer. We will not count this as one of their attempts');
+    }
+} // end while loop for user attempts
+
+// ask user seventh question that has multiple correct possible answers
+var array = ['item1', 'item2', 'item3', 'item4'];
+var flag = false;
+var attemptMax = 6; 
+
+for (var j = 0; j < attemptMax && flag === false; j++) { // I think the flag test is not needed
+    var answer7 = prompt('Do you know an array item?').toLowerCase();
+    console.log('user said ' + answer7 + ' for Q7');
+    for (var k = 0; k < array.length; k++) {
+        if (answer7 === array[k]) { // maybe a problem if array[i] is not lower case
+            alert('You are correct');
+            console.log('user is correct!');
+            flag = true; 
+            j = attemptMax; 
+            k = array.length; 
+        } 
+    } // end for loop testing against the array
+} // end for loop for user getting multiple attempts
+
+if (flag === true) {
+    console.log('user is correct!');
+} else {
+    console.log('user is still wrong after ' + j + ' attempts'); // I would thik we would want to limit the scoop of j to just the loop, but we gave it bigger scope
+    alert('You have used all ' + attemptMax + ' of your attempts and have not gotten it. ')
+}
+// after user is correct, or exhausts 6 tries, report all possible correct answers. 
+var arrayList = '';
+for (i = 0; i < array.length; i++) {
+    arrayList += array[i] + ', ';
+}
+arrayList = arrayList.slice(0,-2); // I got this from google search and result on stackoverflow.com
+alert('Any of the following would be a correct answer: ' + arrayList);
+// alert('You got ' + correctCount + ' correct answers out of 7 questions.');
+
