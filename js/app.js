95a8c6485ce0b9aa2903d60e238b1e85dfbaa942
Christopher L Chapman <ChristopherLChapman42@gmail.com>
2018-05-02 16:47:28 -0700

    add counter for correct answers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var correctCount = 0; 
+
 // Ask for users name
 var userName = prompt('Yo, what is your name?');
 // Greet the user by name in an alert
@@ -13,6 +15,7 @@ var answer1 = prompt('Do you think I have any siblings?').toLowerCase();
 // tell user if they are wrong or right
 if (answer1 === 'yes' || answer1 === 'y') {
     // right answer
+    correctCount++;
     alert('You know it!');
     console.log('user correctly said yes to number 1');
 } else {
@@ -25,6 +28,7 @@ if (answer1 === 'yes' || answer1 === 'y') {
 var answer2 = prompt('Do I like ice cream?').toLowerCase();
 if (answer2 === 'yes' || answer2 === 'y') {
     // correct result 
+    correctCount++;
     alert('Correct. Of course I do!');
     console.log('user correctly said yes to number 2');
 } else {
@@ -37,6 +41,7 @@ if (answer2 === 'yes' || answer2 === 'y') {
 var answer3 = prompt('Do I like country music?').toUpperCase();
 if (answer3 === 'NO' || answer3 === 'N') {
     // correct answer
+    correctCount++;
     alert('Yea, that is fine if other people like it, but it\'s not for me');
     console.log('user understands I am not a country fan (Q3)');
 } else {
@@ -55,6 +60,7 @@ if (answer4 === 'YES' || answer4 === 'Y') {
     console.log('user is wrong on question 4');
 } else {
     // correct answer
+    correctCount++;
     alert('Yes! I have kept "vampire hours" most of my life!');
     console.log('user knows that it has been hard to tell if I am a vampire (Q4)');
 }
@@ -63,6 +69,7 @@ if (answer4 === 'YES' || answer4 === 'Y') {
 var answer5 = prompt('Do I like partner dancing?').toLowerCase();
 if (answer5 === 'y' || answer5 === 'yes') {
     // correct answer
+    correctCount++;
     alert('Of course that is right! I\'ve spent almost all of my life involved in swing dancing!');
     console.log('user figures I would not have spent my life on things I hate (Q5)');
 } else {
@@ -82,6 +89,7 @@ while (i < toManyTries) {
     console.log('user answered '+ answer6 + ' on attempt ' + i + ' for Q6');
     // test if they got it right on this try, or tell them if they are under or over. 
     if (answer6 === '6') {
+        correctCount++;
         alert('You are correct! You figured that out in ' + i + ' attempts');
         console.log('user is correct on attempt ' + i + '!');
         i = toManyTries;
@@ -109,6 +117,7 @@ for (var j = 0; j < attemptMax && flag === false; j++) { // I think the flag tes
     console.log('user said ' + answer7 + ' for Q7');
     for (var k = 0; k < array.length; k++) {
         if (answer7 === array[k]) { // maybe a problem if array[i] is not lower case
+            correctCount++;
             alert('You are correct');
             console.log('user is correct!');
             flag = true; 
@@ -131,5 +140,5 @@ for (i = 0; i < array.length; i++) {
 }
 arrayList = arrayList.slice(0,-2); // I got this from google search and result on stackoverflow.com
 alert('Any of the following would be a correct answer: ' + arrayList);
-// alert('You got ' + correctCount + ' correct answers out of 7 questions.');
+alert('You got ' + correctCount + ' correct answers out of 7 questions.');
 
